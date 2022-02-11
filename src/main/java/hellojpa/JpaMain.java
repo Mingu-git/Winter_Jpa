1f498217f57a3b504d2cab65a23aa48ed6836a16
minukheo1022@gmail.com <minukheo1022@gmail.com>
2022-02-12 04:40:59 +0900

    0212

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,30 +4,78 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
+//가장 중요한건 JPA의 모든 데이터 변경은 트랜잭션 안에서 실행 되어야 한다!!
 public class JpaMain {
     public static void main(String[] args) {
-        //로딩 시점에 하나만 생성
+        //db당 하나 , 웹서버가 올라오는 동안에 하나만 생성.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello") ;
         //persistence.xml에 저장한 unit name : hello
         //일단 db에 연결된상태
 
         //트랙잭션 행동 /즉하나의 행동을 할때 마다 이e/ ntitiy manage 를 생성 해줘야함.
+        //고객의 요청이 올때 썻다가 버렸다가 하는 느낌 -> 따라서 쓰데르간에 공유 x(사용하고 버려야함)
+        //영속성 컨텍스트도 새로 생성이 되서 실행
         EntityManager em = emf.createEntityManager(); //ex) db 커넥션을 얻었다 예시
 
         EntityTransaction tx = em.getTransaction();
+
+
         tx.begin();
-        //멤버 저장
-        // 모든 정보를 변경하는 과정에서는 JPA는 트랜잭션을 부여해줘야함
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
-        em.persist(member);
+        // 항상 트라이 캐치 는 중요한 에러 캐리에 중요한부분 / 따라서 이게 정석 코드 실제에서 이렇게 안씀 why? 스프링이 해줌
+        try {
+            //멤버 저장
+            // 모든 정보를 변경하는 과정에서는 JPA는 트랜잭션을 부여해줘야함
+            Member member = new Member();
+            //member.setId("2L");
+            member.setUsername("HelloB");
+            em.persist(member);
 
-        tx.commit();
+            //영속 -> 영속성 컨텍스트를 통해 멤버가 관리 -> 쓰기지연 sql저장소에 저장이됨 =쿼리가 나가지않고 저장만
+            // 쿼리를 날린다 = db와 상호적용
+            // em.persist(member);
+            // em.persist(member2);
 
-        em.close();
+            // 동일 성보장 2개가 똑같음  A==B -> true IN 동잁 트랜잭션
+            //ex2) 2번조회시 -> 1.1차캐시없음 2.디비 조회후 1차캐시 넣어놈 3.따라서 2번째 호출은 1차캐쉬에서 처리 /별로안씀 컨셉의 이점 존재
+            //Member memberA = em.find(Member.class,1L); //디비조회 -> 쿼리 나감
+            // Member memberB = em.find(Member.class,1L);// 1차캐쉬 -> 쿼리 안나감
 
+
+
+            //멤버 찾기
+            //Member findMember = em.find(Member.class,1L);
+            //System.out.println("findMember.id ###@@=  " + findMember.getName() );
+
+
+            //삭제
+            //em.remove(findMember);
+
+            //수정 -> update 쿼리를 날려서 작동
+            //findMember.setName("Hellominuk");
+
+
+
+            //멤버 객체를 다 가져오는 쿼리 + 그걸 리스트로
+            //JPQL + 페이징 하는게 좋음 5~8 페이지 -> 객체지향 쿼리 + 각디비에 맞게 방언 번역
+            //List<Member> result = em.createQuery("select m from Member as m",Member.class).getResultList();
+            //for(Member member : result) {
+            //    System.out.println("findMember.id ###@@=  " + member.getId());
+            //}
+
+
+
+            tx.commit();
+
+
+        }catch (Exception e) {
+            //다시 이전으로 복원
+            tx.rollback();
+        }finally {
+            //꼭 닫아주어야함
+            em.close();
+        }
         emf.close();
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,15 +1,68 @@
 package hellojpa;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
-@Entity // 꼭 넣어야 JPA가 인식
+@Entity // 꼭 넣어야 JPA가 인식 + 객체 테이블과 매핑을 위한
+@SequenceGenerator(
+        name = "MEMBER SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1,allocationSize = 50 //1부터 시작 1증가
+)
+//@Table(name = "user") 테이블 이름이 다르면 이렇게 매핑 해서 처는 / user테이블로 from절을 보냄
 public class Member {
 
+    //pk 매
     @Id // import javax.persistence 로 넣어야 된댜.
+    //generator를 다음과같이 바꿔주면 테이블마다 시퀀스를 따로 관리할수있다.
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "MEMBER SEQ_GENERATOR") //identity sequece table 3개 존제 (auto 는 3중에 알아서 db방언에 맞게선택)
+    //sequence 하이버네이트 자체 시퀸스 사용해서 순서출력
     private  Long id;
-    private  String name;
+
+    // 객체에는 Username but db에는 name사용
+    //nullable -> not null 유용
+    //length ->길이조절 , columnDefinition  객체에서 디비에 정보설정
+    @Column(name = "name" , nullable = false) //네임을 따로 하고 싶으면 네임으로 따로 매핑
+    private  String username;
+
+    //숫자가 클때는 precision or scale 작은 숫자 쓸떄 정확도 상승
+    private Integer age;
+
+    //db에는 Enum x -> Enumerate 이용
+    //기본이 oridinal : enum의 순서를 저장->type int,저장 0 (user) / string enum의 이름 저장
+    //따라서 대부분 string으로 사용
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    //타입 3가지 -> date(날짜) time(시간) timestamp(둘다)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    //요즘에는 이렇게 날짜씀
+    private LocalDate localDate;
+    private LocalDateTime localDateTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    //db에 varchar를 넘어서는 큰값을 넣고 싶을때 다음과 같이 lob 활용
+
+    @Lob // 문자인 경우엔 clob으로 생성
+    //나머지는 blob매핑
+    private  String description;
+
+    //db에서는 안쓰고 객체에서만 쓰고 싶어
+    //필드 매핑 x ,저장 ,조회x
+    @Transient
+    private int temp;
+
+    //JPA는 동적으로 객체 생성해야되기때문에 기본생성자 하나 만들어줘야함
+
+    public  Member(){
+    }
 
     public Long getId() {
         return id;
@@ -19,11 +72,51 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
